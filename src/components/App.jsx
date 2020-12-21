5517edbe56bda0f32b28680d23c7abbb30c6b50c
Karthik Hebbar <Karthik.HebbarMN@betsol.com>
2020-12-21 19:07:54 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ComingSoon from './screens/ComingSoon'
+
+const App =()=> {
+return(
+    <div style={{height:"100%", display:"flex", width:"100%"}}>
+        <ComingSoon />
+    </div>
+);
+
+}
+
+export default App;
